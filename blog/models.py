21c4a22f206e66f5c16bc359b822bf239a44002b
kirkwater <hansung1459@gmail.com>
2022-08-10 16:28:32 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,6 @@ class Post(models.Model):
     # author: 추후 작성 예정
     def __str__(self):
         return f'[{self.pk}] {self.title}'
+
+    def get_absoulute_url(self):
+        return f'/blog/{self.pk}/'
