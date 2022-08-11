59b89ccaca242a82eb6a513b25dac864b56a3470
kirkwater <hansung1459@gmail.com>
2022-08-11 16:15:30 +0900

    미리보기 요약문

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ import os
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 30) #문자를 담는 필드를 만든다.
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField() #문자열의 길이 제한없는 TextField를 사용해 본문필드 만듬
 
     created_at = models.DateTimeField(auto_now_add = True) #월,일,시,분,초를 기록할 수 있는
@@ -10,6 +11,7 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to = 'blog/images/%Y/%m/%d/', blank = True) #blank는 null값 드가도 괜찮냐?
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)  # blank는 null값 드가도 괜찮냐?
+
     # author: 추후 작성 예정
     def __str__(self):
         return f'[{self.pk}] {self.title}'
