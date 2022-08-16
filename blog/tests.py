704143ab90500a35ecbfc287f4bb6993dcd9b240
kirkwater <hansung1459@gmail.com>
2022-08-16 09:34:15 +0900

    내비게이션 바와 푸터 모듈화

--- blog/tests.py
+++ blog/tests.py
@@ -4,15 +4,31 @@ from .models import Post
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text = '스마트 부산')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEqual(soup.title.text, 'Blog')
-        navbar = soup.nav
+        self.assertEqual('Blog', soup.title.text)
 
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id = 'main-area')
@@ -52,10 +68,6 @@ class TestView(TestCase):
         response = self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code , 200)
         soup = BeautifulSoup(response.content, 'html.parser')
-
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
 
         self.assertIn(post_001.title , soup.title.text)
 
