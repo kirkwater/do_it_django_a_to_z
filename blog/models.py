b781f5842a437b55efe43d69af6a200332b0aae1
kirkwater <hansung1459@gmail.com>
2022-08-16 10:18:40 +0900

    작성자 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 30) #문자를 담는 필드를 만든다.
-    hook_text = models.CharField(max_length=100, blank=True)
+    hook_text = models.CharField(max_length=100, blank=True)  #요약내용
     content = models.TextField() #문자열의 길이 제한없는 TextField를 사용해 본문필드 만듬
 
     created_at = models.DateTimeField(auto_now_add = True) #월,일,시,분,초를 기록할 수 있는
@@ -12,9 +14,9 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to = 'blog/images/%Y/%m/%d/', blank = True) #blank는 null값 드가도 괜찮냐?
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)  # blank는 null값 드가도 괜찮냐?
 
-    # author: 추후 작성 예정
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #on_delete = models.cascade: 작성자가 탈퇴를 하면은 작성한 글도 삭제할 것이다 라는 의미.
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title}::{self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
