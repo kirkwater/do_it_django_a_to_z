2c791806e71bd580d0dbe471bd8ce581da1fd94e
kirkwater <hansung1459@gmail.com>
2022-08-18 11:17:04 +0900

    텍스트로 태그 수정

--- blog/views.py
+++ blog/views.py
@@ -59,7 +59,7 @@ class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
             return redirect('/blog/')
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload','category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload','category']
 
     template_name = 'blog/post_update_form.html'
 
@@ -68,6 +68,37 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = '; '.join(tags_str_list)
+
+        return context
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear() # 다시 지우고 가져오기 위해 넣어야 한다.
+
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name = t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode = True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
+
 
 def category_page(request, slug):
     if slug == 'no_category':
